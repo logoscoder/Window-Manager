ca84deed5da6107b3f3a8d1a3e63a2463e1ba884
Jessé Silva <jessesilvatrabalho@gmail.com>
2018-04-02 15:07:11 -0300

    V1

---
+++ core.js
@@ -0,0 +1,25 @@
+
+var log = function(x){console.log(x);};
+var counter = 0;
+
+function MainThread () {
+  
+  // Cria barra de ferramentas.
+  var toolbar = new Toolbar();
+  toolbar.show();
+  
+  document.body.style.backgroundImage = 'url("background.jpg")';
+  
+  // Cria janela de exemplo.
+  $('#add-window').on('click', function(){
+    var wm = new Window(300, 300, 'Janela '+ counter);
+    wm.content = '<center>Hello World!</center>';
+    wm.show();
+    counter++;
+  });
+}
+
+window.onload = function () {
+  log('LogOS started!');
+  MainThread();
+};
